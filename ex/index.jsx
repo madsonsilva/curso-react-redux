60b0d38a50a4f7080e3442cb7ddbd21ef832aa4d
Madson Silva <madsonsilva@gmail.com>
2017-05-04 01:57:11 -0300

    component Family utilizando um {props.children} e outro com React.cloneElement

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
-    <SilvaFamily />
+    <Family lastname='Silva'>
+        <Member name='Madson' />
+    </Family>
 , document.getElementById('app'))
 
